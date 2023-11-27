b0918153d0824e263ace098c3cabd71b6a253f02
bramahendramahendra <bramahendramahendra@gmail.com>
2023-11-27 08:45:27 +0700

    make Api controller & request for customer

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    public function index() {
-        
-    }
+    //
 }
